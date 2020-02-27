88b1fe8a48717ed8422ab460f5ecc2f16ecb288f
marjorie.galvez@catolica.edu.sv <marjorie.galvez@catolica.edu.sv>
2020-02-27 07:21:41 -0600

    Solucion

---
+++ PortafolioMarjorieGalvez/Practica 1/Form4.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Practica_1
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnNueva = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblSub = new System.Windows.Forms.Label();
+            this.lblImpuesto = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.cboLibros = new System.Windows.Forms.ComboBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtSub = new System.Windows.Forms.TextBox();
+            this.txtImp = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(53, 268);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnNueva
+            // 
+            this.btnNueva.Location = new System.Drawing.Point(191, 268);
+            this.btnNueva.Name = "btnNueva";
+            this.btnNueva.Size = new System.Drawing.Size(75, 23);
+            this.btnNueva.TabIndex = 1;
+            this.btnNueva.Text = "Nueva";
+            this.btnNueva.UseVisualStyleBackColor = true;
+            this.btnNueva.Click += new System.EventHandler(this.btnNueva_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(325, 268);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(28, 34);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(77, 13);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Título del libro:";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(28, 70);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(40, 13);
+            this.lblPrecio.TabIndex = 4;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(28, 108);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(52, 13);
+            this.lblCantidad.TabIndex = 5;
+            this.lblCantidad.Text = "Cantidad:";
+            this.lblCantidad.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblSub
+            // 
+            this.lblSub.AutoSize = true;
+            this.lblSub.Location = new System.Drawing.Point(28, 170);
+            this.lblSub.Name = "lblSub";
+            this.lblSub.Size = new System.Drawing.Size(49, 13);
+            this.lblSub.TabIndex = 6;
+            this.lblSub.Text = "Subtotal:";
+            // 
+            // lblImpuesto
+            // 
+            this.lblImpuesto.AutoSize = true;
+            this.lblImpuesto.Location = new System.Drawing.Point(28, 196);
+            this.lblImpuesto.Name = "lblImpuesto";
+            this.lblImpuesto.Size = new System.Drawing.Size(82, 13);
+            this.lblImpuesto.TabIndex = 7;
+            this.lblImpuesto.Text = "Impuesto (13%):";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(28, 233);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(73, 13);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "Total a pagar:";
+            // 
+            // cboLibros
+            // 
+            this.cboLibros.FormattingEnabled = true;
+            this.cboLibros.Items.AddRange(new object[] {
+            "Elenor y Park",
+            "Es cierto que el amor lo cambia todo, todo?",
+            "Harry Potter y la piedra filosofal",
+            "El mundo de Sofía",
+            "Luz negra"});
+            this.cboLibros.Location = new System.Drawing.Point(124, 34);
+            this.cboLibros.Name = "cboLibros";
+            this.cboLibros.Size = new System.Drawing.Size(121, 21);
+            this.cboLibros.TabIndex = 9;
+            this.cboLibros.SelectedIndexChanged += new System.EventHandler(this.cboLibros_SelectedIndexChanged);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(140, 67);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 10;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(140, 101);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 11;
+            // 
+            // txtSub
+            // 
+            this.txtSub.Location = new System.Drawing.Point(140, 163);
+            this.txtSub.Name = "txtSub";
+            this.txtSub.Size = new System.Drawing.Size(100, 20);
+            this.txtSub.TabIndex = 12;
+            // 
+            // txtImp
+            // 
+            this.txtImp.Location = new System.Drawing.Point(140, 189);
+            this.txtImp.Name = "txtImp";
+            this.txtImp.Size = new System.Drawing.Size(100, 20);
+            this.txtImp.TabIndex = 13;
+            this.txtImp.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(140, 226);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 14;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 325);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtImp);
+            this.Controls.Add(this.txtSub);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.cboLibros);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblImpuesto);
+            this.Controls.Add(this.lblSub);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnNueva);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form4";
+            this.Text = "Librería Universitaria";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnNueva;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblSub;
+        private System.Windows.Forms.Label lblImpuesto;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.ComboBox cboLibros;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtSub;
+        private System.Windows.Forms.TextBox txtImp;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
---
+++ PortafolioMarjorieGalvez/Practica 1/Form4.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboLibros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboLibros.SelectedItem.Equals("Elenor y Park"))
+            {
+                txtPrecio.Text = "15.99";
+            }
+            else if (cboLibros.SelectedItem.Equals("Es cierto que el amor lo cambia todo, todo?"))
+            {
+                txtPrecio.Text = "20.00";
+
+            }
+            else if (cboLibros.SelectedItem.Equals("Harry Potter y la piedra filosofal"))
+            {
+                txtPrecio.Text = "30.00";
+            }
+            else if (cboLibros.SelectedItem.Equals("El mundo de Sofía"))
+            {
+                txtPrecio.Text = "25.98";
+            }
+            else if (cboLibros.SelectedItem.Equals("Luz negra"))
+            {
+                txtPrecio.Text = "9.98";
+            }
+            else
+            {
+                txtPrecio.Text = "30.99";
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double sub, total, imp;
+            if (cboLibros.SelectedItem.Equals("Elenor y Park"))
+            {
+                sub = Convert.ToDouble(txtPrecio.Text) * Convert.ToInt32(txtCantidad.Text);
+                imp = sub * 0.13;
+                total = sub + imp;
+                txtSub.Text = sub.ToString("C2");
+                txtImp.Text = imp.ToString("C2");
+                txtTotal.Text = total.ToString("C2");
+            }
+            else if (cboLibros.SelectedItem.Equals("Es cierto que el amor lo cambia todo, todo?"))
+            {
+                sub = Convert.ToDouble(txtPrecio.Text) * Convert.ToInt32(txtCantidad.Text);
+                imp = sub * 0.13;
+                total = sub + imp;
+                txtSub.Text = sub.ToString("C2");
+                txtImp.Text = imp.ToString("C2");
+                txtTotal.Text = total.ToString("C2");
+            }
+            else if (cboLibros.SelectedItem.Equals("Harry Potter y la piedra filosofal"))
+            {
+                sub = Convert.ToDouble(txtPrecio.Text) * Convert.ToInt32(txtCantidad.Text);
+                imp = sub * 0.13;
+                total = sub + imp;
+                txtSub.Text = sub.ToString("C2");
+                txtImp.Text = imp.ToString("C2");
+                txtTotal.Text = total.ToString("C2");
+            }
+            else if (cboLibros.SelectedItem.Equals("Harry Potter y la piedra filosofal"))
+            {
+                sub = Convert.ToDouble(txtPrecio.Text) * Convert.ToInt32(txtCantidad.Text);
+                imp = sub * 0.13;
+                total = sub + imp;
+                txtSub.Text = sub.ToString("C2");
+                txtImp.Text = imp.ToString("C2");
+                txtTotal.Text = total.ToString("C2");
+            }
+            else if (cboLibros.SelectedItem.Equals("El mundo de Sofía"))
+            {
+
+                sub = Convert.ToDouble(txtPrecio.Text) * Convert.ToInt32(txtCantidad.Text);
+                imp = sub * 0.13;
+                total = sub + imp;
+                txtSub.Text = sub.ToString("C2");
+                txtImp.Text = imp.ToString("C2");
+                txtTotal.Text = total.ToString("C2");
+
+            }
+            else if (cboLibros.SelectedItem.Equals("Luz negra"))
+            {
+                sub = Convert.ToDouble(txtPrecio.Text) * Convert.ToInt32(txtCantidad.Text);
+                imp = sub * 0.13;
+                total = sub + imp;
+                txtSub.Text = sub.ToString("C2");
+                txtImp.Text = imp.ToString("C2");
+                txtTotal.Text = total.ToString("C2");
+            }
+        }
+
+        private void btnNueva_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Text = "";
+            txtImp.Text = "";
+            txtPrecio.Text = "";
+            txtSub.Text = "";
+            txtTotal.Text = "";
+            cboLibros.Text = "";
+        }
+    }
+}
